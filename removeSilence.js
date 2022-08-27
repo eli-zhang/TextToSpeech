5326dc0f2ff87fd51c6a01461433e951211ecdc2
eli-zhang <zeliboid8@gmail.com>
2022-08-27 18:35:32 -0400

    Add code to split up words and save JSON

--- removeSilence.js
+++ removeSilence.js
@@ -1,13 +1,17 @@
 let ffmpeg = require('fluent-ffmpeg');
-
-const file = 'recordings/20220711T023522928Z_recording.wav';
+const fs = require('fs');
+const path = require('path');
+// const file = 'recordings/20220719T190616262Z_recording.wav';
 const filter = 'silencedetect=n=-30dB:d=2';  // Look for silences of duration 2, with -30 dB as the threshold
+const DIRECTORY = "recordings"
 
-let timestampsOfSound = [];
-let startTime;
-let endTime;
+const runCommandOnFile = (file) => {
+    let timestampsOfSilence = [];
+    let timestampsOfSound = [];
+    let startTime;
+    let endTime = 0;
 
-let command = ffmpeg(file)
+    ffmpeg(file)
     .audioFilters(filter)
     .format(null)
     .on('error', function(err) {
@@ -15,6 +19,7 @@ let command = ffmpeg(file)
     })
     .on('end', function() {
         console.log('Processing finished !');
+        splitAudioByTimestamps(file, timestampsOfSound);
     })
     .on('stderr', function(stderr) {
         if (stderr.includes("silencedetect @")) {
@@ -23,11 +28,87 @@ let command = ffmpeg(file)
 
             if (stderr.includes(startString)) {
                 const startStringEndIndex = stderr.indexOf(startString) + startString.length + 1;
-                console.log("Start", stderr.substring(startStringEndIndex))// stderr.indexOf(" ", startStringEndIndex + 1)));
+                startTime = stderr.substring(startStringEndIndex);
+                timestampsOfSound.push({ startTime: endTime, endTime: startTime });     // old end time of silence is start time of
             } else if (stderr.includes(endString)) {
                 const endStringEndIndex = stderr.indexOf(endString) + endString.length + 1;
-                console.log("End", stderr.substring(endStringEndIndex, stderr.indexOf(" ", endStringEndIndex + 1)));
+                endTime = stderr.substring(endStringEndIndex, stderr.indexOf(" ", endStringEndIndex + 1));
+                timestampsOfSilence.push({ startTime, endTime });
             }
         }
     })
     .save('dummy_output');  // No output, so not actually saved anywhere
+}
+
+
+const loopFiles = async (dir) => {
+    try {
+        const files = await fs.promises.readdir(dir);
+    
+        for (const file of files) {
+            const p = path.join(dir, file);
+            const stat = await fs.promises.stat(p);
+        
+            if (stat.isFile()) {
+                console.log("'%s'  file.", p);
+                runCommandOnFile(p);
+            } else if (stat.isDirectory()) {
+                console.log("'%s' directory.", p);
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+loopFiles(DIRECTORY);
+
+const splitAudioByTimestamps = async (file, timestampsOfSound) => {
+    const originalTimestamp = file.substring(file.indexOf("/"), file.indexOf("_"));
+    
+    // Create path to write recordings to.
+    if (!fs.existsSync(`${DIRECTORY}/${originalTimestamp}`)) {
+        fs.mkdirSync(`${DIRECTORY}/${originalTimestamp}`, { recursive: true });
+    }
+
+    let promises = []
+
+    timestampsOfSound.forEach((timestamp) => {
+        let { startTime, endTime } = timestamp;
+
+        if (endTime - startTime < 0.3) {
+            return;     // Skip clips that are shorter than 0.3 seconds
+        }
+
+        const fileName = path.join(
+            DIRECTORY,
+            originalTimestamp,
+            `${startTime}_${endTime}`
+            .replace(/[^0-9a-zA-Z]+/g, '')
+            .concat('.wav')
+        );
+
+        let prom = new Promise((resolve, reject) => {
+            ffmpeg(file)
+            .setStartTime(startTime)
+            .setDuration(endTime - startTime) 
+            .on("error", function(err) {
+                console.log("error: ", +err);
+                reject(err);
+            })
+            .on("end", function(err) {
+                if (!err) {
+                    console.log("Cropping done.");
+                    resolve();
+                }
+            })
+            .saveToFile(fileName);
+        })
+
+        promises.push(prom);
+    })
+
+    await Promise.all(promises);
+    console.log("Done processing all files!");
+    fs.unlinkSync(file);
+}
---
+++ transcribeAudio.js
@@ -0,0 +1,126 @@
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+const path = require('path');
+let ffmpeg = require('fluent-ffmpeg');
+
+// Creates a client
+const client = new speech.SpeechClient();
+const fileName = 'recordings/20220711T022903911Z/327442336727.wav';
+const DIRECTORY = "recordings"
+
+
+const CONFIDENCE_THRESHOLD = 0.9
+const START_TIME_PADDING = 0.05;
+const END_TIME_PADDING = 0.05;
+
+
+async function transcribeAudio() {
+
+  let words = [];
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: fs.readFileSync(fileName).toString('base64'),
+  };
+
+  const config = {
+    enableWordTimeOffsets: true,
+    enableWordConfidence: true,
+    languageCode: 'en-US',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  // Detects speech in the audio file. This creates a recognition job that you
+    // can wait for now, or get its result later.
+    const [operation] = await client.longRunningRecognize(request);
+
+    // Get a Promise representation of the final result of the job
+    const [response] = await operation.promise();
+
+    response.results.forEach(result => {
+        // console.log(`Transcription: ${result.alternatives[0].transcript}`);
+        result.alternatives[0].words.forEach(wordInfo => {
+        // NOTE: If you have a time offset exceeding 2^32 seconds, use the
+        // wordInfo.{x}Time.seconds.high to calculate seconds.
+        const startSecs =
+            `${wordInfo.startTime.seconds}` +
+            '.' +
+            wordInfo.startTime.nanos / 100000000;
+        const endSecs =
+            `${wordInfo.endTime.seconds}` +
+            '.' +
+            wordInfo.endTime.nanos / 100000000;
+        if (wordInfo.confidence > CONFIDENCE_THRESHOLD) {
+            words.push({word: wordInfo.word, startTime: startSecs, endTime: endSecs, confidence: wordInfo.confidence});
+        }
+        });
+    });
+
+    const wordsJSON = JSON.stringify(words);
+    const originalTimestamp = fileName.substring(fileName.indexOf("/") + 1, fileName.indexOf("."));
+    // Create path to write recordings to.
+    if (!fs.existsSync(`${DIRECTORY}/${originalTimestamp}`)) {
+        fs.mkdirSync(`${DIRECTORY}/${originalTimestamp}`, { recursive: true });
+    }
+
+    fs.writeFile(`${DIRECTORY}/${originalTimestamp}.json`, wordsJSON, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log(`JSON data for ${originalTimestamp} saved.`);
+    });
+
+  splitAudioIntoWords(fileName, words);
+}
+
+const splitAudioIntoWords = async (file, words) => {
+    const originalTimestamp = file.substring(file.indexOf("/") + 1, file.indexOf("."));
+    
+    // Create path to write recordings to.
+    if (!fs.existsSync(`${DIRECTORY}/${originalTimestamp}`)) {
+        fs.mkdirSync(`${DIRECTORY}/${originalTimestamp}`, { recursive: true });
+    }
+
+    let promises = []
+
+    words.forEach((wordInfo) => {
+        let { word, startTime, endTime } = wordInfo;
+
+        const fileName = path.join(
+            DIRECTORY,
+            originalTimestamp,
+            word
+            .replace(/[^0-9a-zA-Z]+/g, '')
+            .concat('.wav')
+        );
+
+        let prom = new Promise((resolve, reject) => {
+            ffmpeg(file)
+            .setStartTime(startTime - START_TIME_PADDING)
+            .setDuration(endTime - startTime + START_TIME_PADDING + END_TIME_PADDING) 
+            .on("error", function(err) {
+                console.log("error: ", +err);
+                reject(err);
+            })
+            .on("end", function(err) {
+                if (!err) {
+                    console.log("Word cropping done.");
+                    resolve();
+                }
+            })
+            .saveToFile(fileName);
+        })
+
+        promises.push(prom);
+    })
+
+    await Promise.all(promises);
+    console.log("Done processing all files!");
+    // fs.unlinkSync(file);
+}
+
+transcribeAudio();
